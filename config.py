25eab8590f737fa157b7b35d83a8ff3c16ad43d6
Max Buck <maxbuckdeveloper@gmail.com>
2021-07-12 09:59:29 -0400

    updates

--- config.py
+++ config.py
@@ -3,29 +3,24 @@
 # Others: Make sure the path to the folder is correct
 ###
 
-username = 'max' # => insert your personal computer username here
-pictures_path = '/Users/{u}/BackgroundByReddit'.format(u=username) # => all pictures in this path will be deleted
+username = 'maxbuck' # => insert your personal computer username here
+pictures_path = f'/Users/{username}/BackgroundByReddit' # => all pictures in this path will be deleted
 
 
 ###
-# Configure subreddits 1-3 with your favorite picture heavy subreddits
+# Configure subreddits with your favorite picture heavy subreddits
 #
 # Suggestions:
 #	/r/earthporn
 #	/r/pics
-#	/r/carporn
 #	/r/teacuppigs
 #	/r/bookporn
-# 	/r/matureladiesboners
 #   /r/wallpaper
 #	/r/art
-#	/r/pics
 #
 # Get creative!
 #
 ###
 
 
-subreddit1 = '/r/wallpaper' 
-subreddit2 = '/r/art'
-subreddit3 = '/r/pics'
+subreddits = ['/r/earthporn']
--- redditbackground.py
+++ redditbackground.py
@@ -3,37 +3,36 @@ import urllib
 import os
 import time
 from random import shuffle
-from config import subreddit1, subreddit2, subreddit3, pictures_path
+from config import subreddits, pictures_path
 
 
-headers = {'User-Agent': 'backgroundbyreddit:v1.0.0 (by /u/wormywormm)'}
-e_resp = requests.get('https://www.reddit.com{s1}/new.json?raw_json=1'.format(s1=subreddit1), headers=headers)
-time.sleep(2)
-a_resp = requests.get('https://www.reddit.com{s2}/new.json?raw_json=1'.format(s2=subreddit2), headers=headers)
-time.sleep(2)
-p_resp = requests.get('https://www.reddit.com{s3}/new.json?raw_json=1'.format(s3=subreddit3), headers=headers)
+headers = {"User-Agent": "backgroundbyreddit:v1.0.0 (by /u/wormywormm)"}
 
+# Download new pictures
+the_children = []
+for subreddit in subreddits:
+    resp = requests.get(f"https://www.reddit.com{subreddit}/new.json?raw_json=1")
+    resp.raise_for_status()
+    the_children += resp.json()["data"]["children"]
+    time.sleep(2)
 
-# Remove existing children except default.
+shuffle(the_children)  # mix up the order of the links
+
+# Remove existing pictures except default.
 pics = os.listdir(pictures_path)
-pics_to_delete = [pic for pic in pics if pic[0] != '.']
-pics_to_delete.pop() # keep one picture so the directory is never empty
-for p in pics_to_delete:
-	os.remove('{path}/{filename}'.format(path=pictures_path, filename=p))
+pics_to_delete = [pic for pic in pics if pic[0] != "."]
+pics_to_delete.pop()  # keep one picture so the directory is never empty
+for filename in pics_to_delete:
+    os.remove(f"{pictures_path}/{filename}")
 
 
-# Download new ones
-if e_resp.status_code == 200 and a_resp.status_code == 200 and p_resp.status_code ==200:
-	the_children = a_resp.json()['data']['children'] + e_resp.json()['data']['children'] + p_resp.json()['data']['children']
-	shuffle(the_children) # mix up the order of the links
-
-	for child in the_children:
-		link = child['data']['url']
-		extention = link.split('.')[-1]
-		pid  = child['data']['id']
-		name_of_local_file = '{path}/{p_id}.{ext}'.format(path=pictures_path, p_id=pid, ext=extention)
-		try:
-			if not 'com' in extention and len(extention)<=4:
-				urllib.urlretrieve(link, name_of_local_file)
-		except Exception as e: # sometimes the extention is not present, wrong, or wack
-			print e
+for child in the_children:
+    link = child["data"]["url"]
+    extention = link.split(".")[-1]
+    pid = child["data"]["id"]
+    name_of_local_file = f"{pictures_path}/{pid}.{extention}"
+    try:
+        if not "com" in extention and len(extention) <= 4:
+            urllib.urlretrieve(link, name_of_local_file)
+    except Exception as e:  # sometimes the extention is not present, wrong, or wack
+        print(e)
